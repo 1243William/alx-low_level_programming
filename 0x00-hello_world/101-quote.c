e3a33cf56b8bc1b25edfa5739a8eeb47b5e7222d
William Ofosu <williamofosu677@gmail.com>
2023-02-17 02:59:33 -0800

    101_updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,12 +6,7 @@
  * followed by a newline, to standard error.
  * * Return: Alwaysw 1.
  */
-
-
-
-
 int main(void)
-
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
