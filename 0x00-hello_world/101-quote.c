fc3193f33f7457cae039b981e69b10f8f041801d
William Ofosu <williamofosu677@gmail.com>
2023-02-17 02:47:08 -0800

    101-quote_updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - A program that prints a line to the standard error
- * Return 1 (success)
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * followed by a newline, to standard error.
+ * * Return: Alwaysw 1.
  */
+
 int main(void)
+
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return(1);
 }	
